e1a6b39e171ae9f65a6f9b709be9ea0ad78dfaac
NOH HYEON-SU <109968069+cielokorea@users.noreply.github.com>
2022-08-16 19:46:23 +0900

    Add files via upload

---
+++ server.R
@@ -0,0 +1,51 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(datasets)
+
+mtcars
+mtcars$am <- factor(mtcars$am, labels = c("Automatic", "Manual"))
+
+shinyServer(function(input, output) {
+  
+  formulaText <- reactive({
+    paste("mpg ~", input$variable)
+  })
+  
+  formulaTextPoint <- reactive({
+    paste("mpg ~", "as.integer(", input$variable, ")")
+  })
+  
+  fit <- reactive({
+    lm(as.formula(formulaTextPoint()), data=mtcars)
+  })
+  
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+  output$mpgBoxPlot <- renderPlot({
+    boxplot(as.formula(formulaText()), 
+            data = mtcars,
+            outline = input$outliers)
+  })
+  
+  output$fit <- renderPrint({
+    summary(fit())
+  })
+  
+  output$mpgPlot <- renderPlot({
+    with(mtcars, {
+      plot(as.formula(formulaTextPoint()))
+      abline(fit(), col=2)
+    })
+  })
+  
+})
